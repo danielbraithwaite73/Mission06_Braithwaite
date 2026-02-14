2a177c159fd3caf3610f5946510d1f262549fe36
Daniel <danielbraithwaite73@gmail.com>
2026-02-13 23:01:20 -0700

    Add project files.

---
+++ mission6assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mission6assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
